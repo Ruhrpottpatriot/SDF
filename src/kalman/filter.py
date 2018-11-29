4b1b6fe3c1bfa34aa89411db7775748054e74245
Robert <s3rologi@uni-bonn.de>
2018-11-29 16:39:22 +0100

    Refactor filter and add retrodiction

--- src/kalman/filter.py
+++ src/kalman/filter.py
@@ -1,32 +1,62 @@
-from numpy import ndarray, array, int32, matrix, exp, transpose, power, invert
+from numpy import ndarray, array, identity, zeros, power, exp, dot, sum, transpose, append, subtract
+from numpy.linalg import inv
 
 class KalmanFilter(object):
-    def __init__(self):
-        d_t = 5
-        theta = 60
-        Id:matrix = matrix([[1,0], [0,1]])
-        Null: matrix = matrix([[0,0],[0,0]])
-        q_max = 1 - exp(-2*d_t/theta)
-        sigma = 50
+    def __init__(self, initialMeasurement: ndarray, initialCovariance: ndarray):        
+        self.States: ndarray = [initialMeasurement]
+        self.Covariances: ndarray = [initialCovariance]
 
-        self.F = matrix([[Id, Id*5, Id*(power(d_t, 2))*0.5],[Null, Id, Id*d_t], [Null, Null, Id*exp((-d_t)/theta)]] )
-        self.D = power(q_max, 2) * matrix([[Null, Null, Null], [Null, Null, Null], [Null, Null]])
-        self.H = matrix([Id, Null, Null])
+        q_max: float = 9.1367
+        d_t:float = 5.0
+        theta:float = 60.0
+        Id:ndarray = identity(2)
+        Null: ndarray = zeros(2,2)
+        sigma:float = 50.0
+
+        # state transition 
+        self.F = ndarray(
+            [
+                [Id, Id*5, Id*(power(d_t, 2))*0.5],
+                [Null, Id, Id*d_t], 
+                [Null, Null, Id*exp((-d_t)/theta)]
+            ])
+        
+        # transition noise
+        self.D = power(q_max, 2) * (1.0 - exp(-2.0 * d_t / theta)) * ndarray(
+            [
+                [Null, Null, Null], 
+                [Null, Null, Null],
+                [Null, Null, Id]
+            ])
+
+        # Mmeasurement matrix
+        self.H = ndarray([Id, Null, Null])
+
+        # measurement noise
         self.R = Id * power(sigma, 2)
 
-    def predict(self, probabilityMatrix: matrix, state: array) -> (matrix, matrix):
-        x_k1_k: matrix = self.F  * state
-        P_k1_k: matrix = self.F * probabilityMatrix * transpose(self.F) + self.D        
-        return (x_k1_k, P_k1_k)
+    def predict(self) -> None:
+        predictedState: ndarray = dot(self.F, self.States[-1])
+        self.States.append(predictedState)
+        
+        predictedCovariance = sum(dot(self.F, dot(self.Covariances[-1], transpose(self.F))), self.D)
+        self.Covariances.append(predictedCovariance)
 
-    def filter(self, currentMeasurement: array, probabilityMatrix: matrix, state: array) -> (matrix, matrix):
-        v = currentMeasurement - (self.H * state)
-        S = self.H * probabilityMatrix * transpose(self.H) + self.R
-        W = probabilityMatrix * (transpose(self.H) * invert(S))
+    def filter(self, measurement: array) -> None:
+        v = subtract(measurement, dot(self.H, self.States[-1]))
+        S = sum(dot(self.H, dot(self.Covariances[-1], transpose(self.H))), self.R)
+        W = dot(self.Covariances[-1], dot(transpose(self.H), inv(S)))
 
-        x_k1_k1 = state + W*v
-        P_k1_k1 = probabilityMatrix - (W * S * transpose(W))
-        return (x_k1_k1, P_k1_k1)
+        self.States[-1] = sum(self.States[-1], dot(W, v))
+        self.Covariances[-1] = subtract(self.Covariances[-1], dot(W, dot(S, transpose(W))))
 
-    def retrodict(self, probabilityMatrix: matrix, state: array) -> (matrix, matrix):
-        pass # Apply measurements to all past positions
+    def retrodict(self) -> None:
+        rangeCount = 10 if len(self.States) > 10 else len(self.States) - 1
+        for i in range(rangeCount):
+            pred_P: ndarray = sum(dot(self.F, dot(self.Covariances[-(i+2)], transpose(self.F))), self.D)
+            pred_X: ndarray = dot(self.F, self.States[-(i+2)])
+
+            W: ndarray = dot(self.Covariances[-(i+2)], dot(self.F, inv(pred_P)))
+
+            self.States[-(i+2)] =  sum(self.States[-(i+2)], dot(W, subtract(self.States[-(i+1)], pred_X)))
+            self.Covariances[-(i+2)] = sum(self.Covariances[-(i+2)], dot(W, dot(subtract(self.Covariances[-(i+1)], pred_P)), transpose(W)))
